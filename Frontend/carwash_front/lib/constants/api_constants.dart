5ca5b74185fd7bc9826358493dbbc796bf081b17
Saman Esmaeili <samanesmaeili01@gmail.com>
2025-11-21 22:30:38 +0330

    fix bug for running web and android

--- Frontend/carwash_front/lib/constants/api_constants.dart
+++ Frontend/carwash_front/lib/constants/api_constants.dart
@@ -1,11 +1,23 @@
 import 'dart:io';
+import 'package:flutter/foundation.dart';
 
 class ApiConstants {
   // Automatic detection of server address (for Android emulator 10.0.2.2 and other locales 127.0.0.1)
   static String get baseUrl {
+    // 1. Check if the application is running in a web browser.
+    if (kIsWeb) {
+      // If on web, use localhost which is accessible directly from the browser context.
+      return 'http://127.0.0.1:8000';
+    }
+
+    // 2. If not on web (i.e., running on Mobile/Desktop), use Platform checks.
+    // The dart:io library is fully supported here and will not throw an error.
     if (Platform.isAndroid) {
+      // Crucial for Android Emulators to reach the host machine.
       return 'http://10.0.2.2:8000';
     }
+
+    // Default URL for iOS Simulator, Windows/macOS desktop, etc.
     return 'http://127.0.0.1:8000';
   }
 
