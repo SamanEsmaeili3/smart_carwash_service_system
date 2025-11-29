0c16b2ae8a69eb151d8bbd6ab70b3c2b048b80a9
hamidreza062 <hrk1383@gmail.com>
2025-11-29 20:27:49 +0330

    Feat: Add URLs for Carwash Services

--- Backend/carwash/urls.py
+++ Backend/carwash/urls.py
@@ -3,20 +3,31 @@ from .views import (
     CarwashApplicationView, 
     AdminPendingCarwashListView,
     AdminCarwashApprovalView,
-    CarwashProfileUpdateView  
+    CarwashProfileUpdateView,
+    CarwashServiceListCreateView, # <-- ویو جدید
+    CarwashServiceDeleteView      # <-- ویو جدید
 )
 
 urlpatterns = [
+    # --- Public ---
     # /api/carwash/apply/
     path('apply/', CarwashApplicationView.as_view(), name='carwash-apply'),
     
+    # --- Admin ---
     # /api/carwash/admin/pending/
     path('admin/pending/', AdminPendingCarwashListView.as_view(), name='admin-pending-list'),
     
     # /api/carwash/admin/manage/<pk>/
     path('admin/manage/<int:pk>/', AdminCarwashApprovalView.as_view(), name='admin-manage-carwash'), 
     
-    # --- NEW: Sprint 2 Task-B2.3 ---
+    # --- Carwash Owner ---
     # /api/carwash/profile/me/
     path('profile/me/', CarwashProfileUpdateView.as_view(), name='carwash-profile-update'),
+
+    # --- NEW: Services (Menu) ---
+    # /api/carwash/services/ (GET list, POST create)
+    path('services/', CarwashServiceListCreateView.as_view(), name='carwash-service-list-create'),
+    
+    # /api/carwash/services/<pk>/ (DELETE)
+    path('services/<int:pk>/', CarwashServiceDeleteView.as_view(), name='carwash-service-delete'),
 ]
