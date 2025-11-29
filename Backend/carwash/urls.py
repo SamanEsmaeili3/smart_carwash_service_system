34463a48ab0dc5aa186955edc7f9add6c6ef8503
hamidreza062 <hrk1383@gmail.com>
2025-11-29 20:35:39 +0330

    Feat: Add Public Carwash List URL

--- Backend/carwash/urls.py
+++ Backend/carwash/urls.py
@@ -4,27 +4,31 @@ from .views import (
     AdminPendingCarwashListView,
     AdminCarwashApprovalView,
     CarwashProfileUpdateView,
-    CarwashServiceListCreateView, # <-- ویو جدید
-    CarwashServiceDeleteView      # <-- ویو جدید
+    CarwashServiceListCreateView,
+    CarwashServiceDeleteView,
+    CustomerCarwashListView 
 )
 
 urlpatterns = [
     # --- Public ---
-    # /api/carwash/apply/
+    # /api/carwash/apply/ (POST)
     path('apply/', CarwashApplicationView.as_view(), name='carwash-apply'),
     
+    # /api/carwash/list/ (GET - For Customers) 
+    path('list/', CustomerCarwashListView.as_view(), name='carwash-list-public'),
+
     # --- Admin ---
-    # /api/carwash/admin/pending/
+    # /api/carwash/admin/pending/ (GET)
     path('admin/pending/', AdminPendingCarwashListView.as_view(), name='admin-pending-list'),
     
-    # /api/carwash/admin/manage/<pk>/
+    # /api/carwash/admin/manage/<pk>/ (POST)
     path('admin/manage/<int:pk>/', AdminCarwashApprovalView.as_view(), name='admin-manage-carwash'), 
     
     # --- Carwash Owner ---
-    # /api/carwash/profile/me/
+    # /api/carwash/profile/me/ (PUT)
     path('profile/me/', CarwashProfileUpdateView.as_view(), name='carwash-profile-update'),
 
-    # --- NEW: Services (Menu) ---
+    # --- Services (Menu) ---
     # /api/carwash/services/ (GET list, POST create)
     path('services/', CarwashServiceListCreateView.as_view(), name='carwash-service-list-create'),
     
