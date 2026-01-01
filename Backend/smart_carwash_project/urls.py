b3f5aaf5ff874d9a253a03e6da2a7d66c5770711
hamidreza062 <hrk1383@gmail.com>
2026-01-02 02:37:42 +0330

    chore(backend): configure Liara Disk volume and setup media static serving

--- Backend/smart_carwash_project/urls.py
+++ Backend/smart_carwash_project/urls.py
@@ -2,14 +2,17 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenRefreshView
 from accounts.views import CustomTokenObtainPairView # <--- Import your new view
+from django.conf import settings               
+from django.conf.urls.static import static     
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
     path('api/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'), 
-    
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'), 
     path('api/accounts/', include('accounts.urls')), 
     path('api/carwash/', include('carwash.urls')),
     path('api/order/', include('orders.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
